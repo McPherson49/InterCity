3f65b4605b5d57a5474a38a96f56de406d661e28
Afolabi Peter <108076216+McPherson49@users.noreply.github.com>
2024-12-20 17:06:41 +0100

    Initial Commit

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -7,8 +7,8 @@ import TravelTips from "../components/TravelTips";
 const Homepage = () => {
   return (
     <div className=" w-full font-mulish">
-      <Nav />
-      <div className=" w-full px-4 md:px-12 lg:px-24">
+      <div className=" w-full px-4 md:px-16 lg:px-48">
+        <Nav />
         <Hero />
         <Compare />
         <TravelTips />
